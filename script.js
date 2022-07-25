0270b236ed3420e9caba8aa1729e890957524b9f
Oda Schneider <oda.schneider@gmx.de>
2022-07-25 12:03:28 +0200

    basic style

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
-function init(){
+async function init(){
+    await includeHTML();
     setURL('http://gruppe-287.developerakademie.net/smallest_backend_ever');
+}
+
+
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html");
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
 }
